19643e3b8a585b41c460bdb569c06e1709a5351f
Jongmin BAEK <rrrgk12@gmail.com>
2026-08-25 13:49:39 +0900

    Time: 16 ms (21.04%), Space: 46.4 MB (42.51%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        return result(s, t);
+    }
+    public boolean result(String s, String t){
+        if(s.length() != t.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i< s.length(); i++){
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(int i = 0; i<t.length(); i++){
+            char c = t.charAt(i);
+
+            if(!map.containsKey(c) || map.get(c) == 0) return false;
+            map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+
+    }
+}

54a169a1b9f5adc01f2b0e024a0cf444b9706a37
Jongmin BAEK <rrrgk12@gmail.com>
2026-09-14 16:04:58 +0900

    Time: 4 ms (92.38%), Space: 82.5 MB (81.68%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        return result(nums);
+    }
+
+    private int result(int[] InNums){
+        int a = InNums[0];
+        int b = InNums[0];
+
+        while(true) {
+            a = InNums[a];
+            b = InNums[InNums[b]];
+            if(a == b) break;       
+        }
+
+        a = InNums[0]; 
+        
+        while(a != b) {
+            a = InNums[a];
+            b = InNums[b];
+            
+
+            if(a == b) break;
+        }
+
+        return a;
+    }
+}
+
+// 한칸씩 움지기는 배열
+// 13 32 41 24 
